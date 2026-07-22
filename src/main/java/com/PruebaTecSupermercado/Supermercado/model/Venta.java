6887329a9a021dd71ea4ecf4cb7f312e8adaeeee
Emmanuel Yapura <eyapura96@gmail.com>
2026-07-22 18:05:00 -0300

    actualiza gitignore

--- src/main/java/com/PruebaTecSupermercado/Supermercado/model/Venta.java
+++ src/main/java/com/PruebaTecSupermercado/Supermercado/model/Venta.java
@@ -1,4 +1,10 @@
 package com.PruebaTecSupermercado.Supermercado.model;
 
+import java.time.LocalDate;
+
+
 public class Venta {
+    private Long id;
+    private LocalDate fecha;
+    private String estado;
 }
