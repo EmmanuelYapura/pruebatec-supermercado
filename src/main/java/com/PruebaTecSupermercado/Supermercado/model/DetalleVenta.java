cf1a4647cb0578ccf3af293fbc639447815b3ced
Emmanuel Yapura <eyapura96@gmail.com>
2026-07-22 18:06:33 -0300

    agrega clases del proyect usando relacion unidireccional

--- src/main/java/com/PruebaTecSupermercado/Supermercado/model/DetalleVenta.java
+++ src/main/java/com/PruebaTecSupermercado/Supermercado/model/DetalleVenta.java
@@ -1,4 +1,29 @@
 package com.PruebaTecSupermercado.Supermercado.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DetalleVenta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //venta
+    @ManyToOne
+    private  Venta venta;
+
+    //producto
+    @ManyToOne
+    private  Producto prod;
+    private Integer cantProd;
+    private Double precio;
 }
--- src/main/java/com/PruebaTecSupermercado/Supermercado/model/Venta.java
+++ src/main/java/com/PruebaTecSupermercado/Supermercado/model/Venta.java
@@ -1,10 +1,27 @@
 package com.PruebaTecSupermercado.Supermercado.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDate;
 
-
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Venta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDate fecha;
     private String estado;
+
+    // cada venta tiene una sucursal
+    @ManyToOne
+    private Sucursal sucursal;
 }
