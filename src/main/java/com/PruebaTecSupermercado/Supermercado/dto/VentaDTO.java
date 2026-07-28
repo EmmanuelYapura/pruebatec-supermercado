80a30193dbd79da9fff590d9a067add9060c2e88
Emmanuel Yapura <eyapura96@gmail.com>
2026-07-28 15:01:20 -0300

    agrega patron Builder en entidad Producto y DTOs para mapeo

--- src/main/java/com/PruebaTecSupermercado/Supermercado/dto/VentaDTO.java
+++ src/main/java/com/PruebaTecSupermercado/Supermercado/dto/VentaDTO.java
@@ -1,9 +1,6 @@
 package com.PruebaTecSupermercado.Supermercado.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -12,6 +9,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class VentaDTO {
     // datos de la venta
     private Long id;
