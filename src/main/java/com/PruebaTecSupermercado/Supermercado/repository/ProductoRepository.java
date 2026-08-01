cac13662377e6c52e8ce5454d730f141f8ec9457
Emmanuel Yapura <eyapura96@gmail.com>
2026-07-31 23:33:31 -0300

    crea metodo para buscar un producto por nombre

--- src/main/java/com/PruebaTecSupermercado/Supermercado/repository/ProductoRepository.java
+++ src/main/java/com/PruebaTecSupermercado/Supermercado/repository/ProductoRepository.java
@@ -3,5 +3,10 @@ package com.PruebaTecSupermercado.Supermercado.repository;
 import com.PruebaTecSupermercado.Supermercado.model.Producto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductoRepository extends JpaRepository<Producto, Long> {
+
+    // metodo para buscar producto
+    Optional<Producto> findByNombre(String nombre);
 }
